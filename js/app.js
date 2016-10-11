b1349b1116b9177b71a9ae76f614b9de6d702dd8
Janos Wortmann <janos.wortmann@web.de>
2016-10-11 05:30:41 +0200

    Material Design

--- js/app.js
+++ js/app.js
@@ -1,91 +1,70 @@
-var count = 2;                        // Zählvariable für die Gegner
+var count = 2;
 
-// Funktion zur Berechnung der Gewinnwahrscheinlichkeiten bei zwei gegebenen TTR-Werten
-function gewinnwahrscheinlichkeit(ttr1, ttr2) {
+function cgw(ttr1, ttr2) {
     return 1/(1+Math.pow(10, (ttr2 - ttr1)/150));
 }
 
 angular.module('myApp', [])
     .controller('myController', ['$scope', function($scope) {
-        $scope.aenderungskonstante = "16";
-        $scope.new_ttr0 = "";
-        $scope.veraenderung0 = "";
+        $scope.ak = "16";
 
-        $scope.opponents = [
-            {nr:1, ttr:"", gw:"", gw_display:"", new_ttr:"", veraenderung:""},
-            {nr:2, ttr:"", gw:"", gw_display:"", new_ttr:"", veraenderung:""}
+        $scope.opp = [
+            {n:1, ttr:"", gw:"", gwr:""},
+            {n:2, ttr:"", gw:"", gwr:""}
+        ];
+
+        $scope.wins = [
+            {n:0, ttr:"", dttr:""},
+            {n:1, ttr:"", dttr:""},
+            {n:2, ttr:"", dttr:""}
         ];
  
-        $scope.addOpponent = function() {
+        $scope.add = function() {
             count++;
-            $scope.opponents.push({nr:count, ttr:"", gw:"", gw_display:"", new_ttr:"", veraenderung:""});
+            $scope.opp.push({n:count, ttr:"", gw:"", gwr:""});
+            $scope.wins.push({n:count, ttr:"", dttr:""});
         };
 
-        $scope.calculate = function() {
+        $scope.calc = function() {
             // alle berechneten Ausgabewerte löschen
-            $scope.new_ttr0 = "";
-            $scope.veraenderung0 = "";
-            for (var i = 0; i < $scope.opponents.length; i++) {
-                $scope.opponents[i].gw = "";
-                $scope.opponents[i].gw_display = "";
-                $scope.opponents[i].new_ttr = "";
-                $scope.opponents[i].veraenderung = "";
+            for (var i = 0; i < $scope.opp.length; i++) {
+                $scope.opp[i].gw = "";
+                $scope.opp[i].gwr = "";
             }
 
-            // Eingabefelder überprüfen
-            if (isNaN($scope.current_ttr) || $scope.current_ttr < 0 || $scope.current_ttr > 9999) {
-                $("input[name='input1']").parent().parent().addClass("has-error");
-            } else {
-                $("input[name='input1']").parent().parent().removeClass("has-error");
+            for (var i = 0; i < $scope.wins.length; i++) {
+                $scope.wins[i].ttr = "";
+                $scope.wins[i].dttr = "";
             }
 
-            for (var i = 0; i < $scope.opponents.length; i++) {
-                var j = i+1;
-                if (isNaN($scope.opponents[i].ttr) || $scope.opponents[i].ttr < 0 || $scope.opponents[i].ttr > 9999) {
-                    $("input[name='opponent" + j + "']").parent().parent().addClass("has-error");
-                } else {
-                    $("input[name='opponent" + j + "']").parent().parent().removeClass("has-error");
-                }
-            }
+            if (!isNaN($scope.ttr0) && $scope.ttr0 > 0) {
+                var nopp = 0;  // Anzahl eingetragener Gegner
+                var gwsum = 0; // Summe der Gewinnwahrscheinlichkeiten
 
-            if (!isNaN($scope.current_ttr) && $scope.current_ttr > 0 && $scope.current_ttr < 10000) {
-                var anz_opponents = 0; // Anzahl eingetragener Gegner
-                var sum_gw = 0;        // Summe der Gewinnwahrscheinlichkeiten
-
-                // Gewinnwahrscheinlichkeit berechnen
-                for (var i = 0; i < $scope.opponents.length; i++) {
-                    if (!isNaN($scope.opponents[i].ttr) && $scope.opponents[i].ttr > 0 && $scope.opponents[i].ttr < 10000) {
-                        anz_opponents++;
-                        // exakte Gewinnwahrscheinlichkeit
-                        $scope.opponents[i].gw = gewinnwahrscheinlichkeit($scope.current_ttr, $scope.opponents[i].ttr);
-                        // Gewinnwahrscheinlichkeiten summieren
-                        sum_gw += $scope.opponents[i].gw;
+                // Schleife über alle Gegner
+                for (var i = 0; i < $scope.opp.length; i++) {
+                    if (!isNaN($scope.opp[i].ttr) && $scope.opp[i].ttr > 0) {
+                        nopp++;
+                        // exakte Gewinnwahrscheinlichkeit berechnen
+                        $scope.opp[i].gw = cgw($scope.ttr0, $scope.opp[i].ttr);
+                        // Gewinnwahrscheinlichkeiten aufsummieren
+                        gwsum += $scope.opp[i].gw;
                         // gerundete Gewinnwahrscheinlichkeit
-                        $scope.opponents[i].gw_display = Math.round($scope.opponents[i].gw * 1000) / 1000;
+                        $scope.opp[i].gwr = (Math.round($scope.opp[i].gw * 1000) / 10).toString().concat(" %");
                     }
                 }
 
                 // neue TTR-Werte berechnen
-                if (anz_opponents > 0 && !isNaN($scope.aenderungskonstante) && $scope.aenderungskonstante > 0) {
-                    // neuer TTR-Wert bei 0 Siegen
-                    $scope.new_ttr0 = Math.round(eval($scope.current_ttr - $scope.aenderungskonstante * sum_gw));
-                    // Veränderung bei 0 Siegen
-                    $scope.veraenderung0 = $scope.new_ttr0 - $scope.current_ttr;
-                    if ($scope.veraenderung0 < 0) {
-                        $scope.veraenderung0 = "(".concat($scope.veraenderung0.toString()).concat(")");
-                    } else {
-                        $scope.veraenderung0 = "(+".concat($scope.veraenderung0.toString()).concat(")");
-                    }
-
-                    // neuer TTR-Wert bei i>0 Siegen
-                    for (var i = 1; i <= anz_opponents; i++) {
-                        $scope.opponents[i-1].new_ttr = Math.round(eval($scope.current_ttr - $scope.aenderungskonstante * (sum_gw - i)));
-                        $scope.opponents[i-1].veraenderung = $scope.opponents[i-1].new_ttr - $scope.current_ttr;
-                        if ($scope.opponents[i-1].veraenderung < 0) {
-                            $scope.opponents[i-1].veraenderung = "(".concat($scope.opponents[i-1].veraenderung.toString()).concat(")");
+                if (nopp > 0) {
+                    for (var i = 0; i <= nopp; i++) {
+                        $scope.wins[i].ttr = Math.round(eval($scope.ttr0 - $scope.ak * (gwsum - i)));
+                        $scope.wins[i].dttr = $scope.wins[i].ttr - $scope.ttr0;
+                        if ($scope.wins[i].dttr < 0) {
+                            $scope.wins[i].dttr = $scope.wins[i].dttr.toString();
                         } else {
-                            $scope.opponents[i-1].veraenderung = "(+".concat($scope.opponents[i-1].veraenderung.toString()).concat(")");
+                            $scope.wins[i].dttr = "+".concat($scope.wins[i].dttr.toString());
                         }
+
                     }
                 }
             }
