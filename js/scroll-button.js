3c59dc36c72785dbdf03f0c15f93311aa4ffb81d
Janos Wortmann <jwortmann@outlook.com>
2019-08-29 17:23:40 +0200

    Update charts

---
+++ js/scroll-button.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('body').append('<a href="#top" id="scroll-top"><span class="btn-floating"><i class="material-icons">keyboard_arrow_up</i></span></a>');
+    $('#scroll-top').hide();
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('#scroll-top').fadeIn();
+            } else {
+                $('#scroll-top').fadeOut();
+            }
+        });
+        $('#scroll-top').click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+});
